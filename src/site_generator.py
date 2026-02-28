f1eb5c916934ca3be577656319510f9f0926ab3c
DanielThedie <dthedie@gmail.com>
2026-02-28 11:28:39 +0000

    WIP for blog interface update

--- src/site_generator.py
+++ src/site_generator.py
@@ -1,6 +1,7 @@
 import yaml
 import markdown
 import re
+import os
 from pathlib import Path
 
 from html_elements import format_image, format_buttons
@@ -16,6 +17,7 @@ class SiteGenerator:
         self.templates = Path(templates_folder)
         self.docs = Path(docs_folder)
         self.base = self.templates / "base.html"
+        self.base_html = self.init_base()
         self.pages = self.read_yaml(self.content / 'pages.yaml')['pages']
 
     def read_html(self, file_path):
@@ -26,6 +28,10 @@ class SiteGenerator:
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(content)
 
+    def init_base(self):
+        latest_blog = self.get_blog_posts()[0]
+        return self.read_html(self.base).replace('<!-- LATEST_BLOG -->', f'\"blog/{latest_blog.stem}.html\"')
+
     def update_html(self, page, placeholder, new_content):
         page_html = self.read_html(self.docs / f"{page}.html")
         page_html = page_html.replace(placeholder, new_content)
@@ -35,6 +41,11 @@ class SiteGenerator:
         with open(template_path, "r", encoding="utf-8") as f:
             template = f.read()
         return template.format(**kwargs)
+
+    def get_blog_posts(self):
+        blog_folder = self.content / 'blog'
+        blog_posts = sorted(blog_folder.glob("*.md"), reverse=True)
+        return blog_posts
 
     def read_yaml(self, yaml_file):
         with open(yaml_file, 'r', encoding='utf-8') as f:
@@ -59,10 +70,9 @@ class SiteGenerator:
         content_html = self.page_wrapper(title, items_html)
         self.update_html(name, "<!-- CONTENT -->", content_html)
 
-    def add_blog(self, name):
-        blog_folder = self.content / name
-        blog_posts = sorted(blog_folder.glob("*.md"), reverse=True)
-        if not (self.docs / f"{name}.html").exists() or not blog_posts:
+    def add_blog(self):
+        blog_posts = self.get_blog_posts()
+        if not blog_posts:
             return
         blog_html = ""
         for post in blog_posts:
@@ -75,18 +85,26 @@ class SiteGenerator:
                              f'Posted on {blog_date}</div>')
             html = re.sub(r'(<h1[^>]*>.*?</h1>)', r'\1' + metadata_html,
                           html, count=1)
-            blog_html += self.fill_html_template(
-                self.templates / f'{name}-item.html', content=html
+            blog_html = self.fill_html_template(
+                self.templates / 'blog-item.html', content=html
                 )
-        content_html = self.page_wrapper(name, blog_html)
-        self.update_html(name, "<!-- CONTENT -->", content_html)
+            content_html = self.page_wrapper('blog', blog_html)
+            self.update_html(f'blog/{post.stem}', "<!-- CONTENT -->", content_html)
 
     def create_pages(self):
+        base_html = self.base_html
         for page in self.pages:
             title = page.get('title', page['name'])
-            base_html = self.read_html(self.base)
             self.write_html(self.docs / f"{page['name']}.html", base_html)
 
             style_html = (f'<link href="styles/{title}.css" '
                           f'rel="stylesheet" />\n')
             self.update_html(page['name'], "<!-- STYLESHEET -->", style_html)
+
+        blog_posts = self.get_blog_posts()
+        out_folder = self.docs / 'blog'
+        if not out_folder.exists: os.mkdir(self.docs / 'blog')
+        for post in blog_posts:
+            blog_style = ('<link href="styles/blog.css" rel="stylesheet" />\n')
+            self.write_html(self.docs / 'blog' / f'{post.stem}.html', base_html)
+            self.update_html(f'blog/{post.stem}', "<!-- STYLESHEET -->", blog_style)
