da91d11fd7a7a693fdbbeae50515de67e69ecb4e
DanielThedie <dthedie@gmail.com>
2026-05-13 09:20:04 +0100

    Improved RSS feed
    * Add post content to feed
    * Use a stylesheet for nice feed preview when clicked
    * Add a link to the RSS feed on blog index

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content'
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
     const posts = await getCollection('blog')
@@ -11,7 +14,11 @@ export async function GET(context) {
 	    title: post.data.title,
 	    pubDate: post.data.pubDate,
 	    link: `/blog/${post.id}/`,
+	    content: sanitizeHtml(parser.render(post.body), {
+		allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+	    }),
 	})),
 	customData: `<language>en-gb</language>`,
+	stylesheet: '/pretty-feed-v3.xsl',
     });
 }
